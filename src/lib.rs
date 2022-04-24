da70d1ea3e7d7605d1fc890e02f2d0b3e5dac924
jath03 <jackthughes@yahoo.com>
2022-04-24 15:02:01 -0700

    Modified timing and added RGBW support

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,53 @@
 
 use cortex_m::asm::delay;
 use embedded_hal::digital::v2::OutputPin;
-use smart_leds_trait::{RGB8, SmartLedsWrite};
+use core::marker::PhantomData;
+use smart_leds_trait::{RGB8, RGBW, SmartLedsWrite};
 
-pub struct Ws2812<P: OutputPin> {
+pub mod devices {
+    pub struct Ws2812;
+    pub struct Sk6812w;
+}
+
+pub struct Ws2812<P: OutputPin, DEVICE = devices::Ws2812> {
     pin: P,
+    device: PhantomData<DEVICE>,
 }
 
 impl<P: OutputPin> Ws2812<P> {
-    pub fn new(pin: P) -> Ws2812<P> {
-        Ws2812 { pin: pin }
+    pub fn new(pin: P) -> Self {
+        Self {
+            pin: pin,
+            device: PhantomData {},
+        }
     }
+}
+
+impl<P: OutputPin> Ws2812<P, devices::Sk6812w> {
+    pub fn new_sk6812w(pin: P) -> Self {
+        Self {
+            pin: pin,
+            device: PhantomData {},
+        }
+    }
+}
+
+impl<P: OutputPin, D> Ws2812<P, D> {
     fn write_byte(&mut self, data: u8) {
         let mut bitmask: u8 = 0x80;
         while bitmask != 0 {
             self.pin.set_high().unwrap_or(());
-            delay(2);
+            delay(5);
             if data & bitmask != 0 {
                 delay(7);
                 self.pin.set_low().unwrap_or(());
             } else {
                 self.pin.set_low().unwrap_or(());
-                delay(2); 
+                delay(2);
             }
             bitmask >>= 1;
         }
-        delay(5);
+        delay(2);
     }
 }
 
@@ -46,6 +68,28 @@ where
             self.write_byte(color.g);
             self.write_byte(color.r);
             self.write_byte(color.b);
+        }
+        Ok(())
+    }
+}
+
+impl<P> SmartLedsWrite for Ws2812<P, devices::Sk6812w>
+where
+    P: OutputPin,
+{
+    type Color = RGBW<u8, u8>;
+    type Error = ();
+    fn write<T, I>(&mut self, iterator: T) -> Result<(), ()>
+    where
+        T: Iterator<Item = I>,
+        I: Into<Self::Color>
+    {
+        for item in iterator {
+            let color: Self::Color = item.into();
+            self.write_byte(color.g);
+            self.write_byte(color.r);
+            self.write_byte(color.b);
+            self.write_byte(color.a.0);
         }
         Ok(())
     }
